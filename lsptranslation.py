7b0612bde909d6325950da3771291d8798463139
unknown <frank@.(none)>
2012-10-25 10:10:20 -0700

    Code clean-up. removed the check box code completly added license and version keying
    Updadted conductor export to require 16384 channel configuration

--- lsptranslation.py
+++ lsptranslation.py
@@ -1,12 +1,34 @@
 #-------------------------------------------------------------------------------
-# Name:        module1
+# Name:        lsptranslation.py
 # Purpose:
 #
 # Author:      frank_reichstein
 #
 # Created:     23/08/2012
 # Copyright:   (c) frank_reichstein 2012
-# Licence:     <your licence>
+# Licence:
+
+"""
+Copyright (C) 2011 by Frank Reichstein
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+"""
 #-------------------------------------------------------------------------------
 #!/usr/bin/env python
 from xml.etree.ElementTree import ElementTree
@@ -439,8 +461,12 @@ class Sequence():
     def outputConductor(self, outfile):
         condData = bytearray(4)
         FH = open(outfile,'wb')
+        chanCount = self.networks.getMaxChannels()
         #Conductor file format:
         #chxinuniverse1
+        if chanCount != 16384:
+           return False
+
         for period in range(self.numPeriods):
             for i in range(4096):
                 for j in range(4):
