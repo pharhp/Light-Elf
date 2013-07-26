4bb26c07395a1a3c722726156b052fde13f9578d
Frank Reichstein <reichstf@gmail.com>
2013-07-25 23:45:35 -0700

    update version number

--- light elf app.py
+++ light elf app.py
@@ -29,7 +29,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 """
-Version = "0.0.9 Beta(custom 1)"
+Version = "0.0.10 Beta"
 
 
 #-------------------------------------------------------------------------------
